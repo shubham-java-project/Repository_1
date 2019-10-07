c63d2417ebd8c482bbb30148b5c8edc36f8d927c
shubham1 <javadev.shubham1995@gmail.com>
2019-10-06 18:11:48 -0700

    adding reverse for loop

--- HelloWorld/src/com/app/model/HelloWrold.java
+++ HelloWorld/src/com/app/model/HelloWrold.java
@@ -3,6 +3,10 @@ package com.app.model;
 public class HelloWrold {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		//adding reverse for loop
+		for(int i=10;i>=0;i--) {
+			System.out.println(i);
+		}
 	}
 
 }
