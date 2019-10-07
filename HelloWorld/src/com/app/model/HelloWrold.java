ea6b934552588d2d88a7b8da36dc90f583b30340
shubham <javadev.shubham1995@gmail.com>
2019-10-06 18:08:39 -0700

    adding for loop

--- HelloWorld/src/com/app/model/HelloWrold.java
+++ HelloWorld/src/com/app/model/HelloWrold.java
@@ -1,8 +1,12 @@
 package com.app.model;
 
 public class HelloWrold {
+	
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		for(int i=0;i<=10;i++) {
+			System.out.println(i);
+		}
 	}
 
 }
